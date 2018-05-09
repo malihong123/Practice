ef4f5f94eefc994820605f96dc05dee578bc4ec5
malihong123 <1147179487@qq.com>
2018-05-09 16:06:38 +0800

    百度前端练习

---
+++ 第一天/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/5/9 0009.
+ */
+var btn=document.getElementById("btn");
+btn.onclick=function(){
+  alert("练习WEB前端标签");
+}
---
+++ 第二天/js/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Administrator on 2018/5/9 0009.
+ */
